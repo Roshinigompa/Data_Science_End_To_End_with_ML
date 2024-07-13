014aad57daaa29407d6d97ad440b2eb4f73d1221
Hyma Roshini Gompa <hymaroshinigompa@Hymas-Laptop.local>
2024-07-13 16:45:46 -0400

    exception,logger & utilities added

---
+++ src/mlProject/utils/common.py
@@ -0,0 +1,134 @@
+#Finctionality that is frequently used in your code is called utils
+# Lets say you want to read a yml file in all of your components instead of writing it everytime in your code its best way you can add it in common so whenever you need it just write import from here
+
+import os
+from box.exceptions import BoxValueError
+import yaml
+from mlProject import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """Reads a yaml file and returns a ConfigBox object.
+
+    Args:
+        path_to_yaml (Path): Path to the yaml file.
+
+    Raises:
+        ValueError: If the yaml file is empty.
+        e: Other exceptions.
+
+    Returns:
+        ConfigBox: ConfigBox object containing yaml content.
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            if not content:
+                raise BoxValueError("Yaml file is empty")
+            return ConfigBox(content)
+    except BoxValueError as e:
+        raise ValueError("Yaml file is empty") from e
+    except Exception as e:
+        raise e
+
+
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """save json data
+
+    Args:
+        path (Path): path to json file
+        data (dict): data to be saved in json file
+    """
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logger.info(f"json file saved at: {path}")
+
+
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+
+    Args:
+        path (Path): path to json file
+
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file loaded succesfully from: {path}")
+    return ConfigBox(content)
+
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """save binary file
+
+    Args:
+        data (Any): data to be saved as binary
+        path (Path): path to binary file
+    """
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at: {path}")
+
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """load binary data
+
+    Args:
+        path (Path): path to binary file
+
+    Returns:
+        Any: object stored in the file
+    """
+    data = joblib.load(path)
+    logger.info(f"binary file loaded from: {path}")
+    return data
+
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
+
+
+
+
